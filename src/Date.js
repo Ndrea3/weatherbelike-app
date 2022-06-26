68e4a600241482b4a8dd7edc452ae0f4cabb4ca6
Ndrea3 <101579085+Ndrea3@users.noreply.github.com>
2022-06-26 18:27:37 -0400

    Updated the Date component
    Lets see if this works better in react

--- src/Date.js
+++ src/Date.js
@@ -1,46 +1,22 @@
-import React from "react";
+import { useEffect, useState } from "react";
 
-export default function Date() {
-    return (
- (date) => {
-            let weekDays = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
-            let weekDay = weekDays[now.getDay()];
 
-            let months = [
-                "January",
-                "February",
-                "March",
-                "April",
-                "May",
-                "June",
-                "July",
-                "August",
-                "September",
-                "October",
-                "November",
-                "December",
-            ];
-            let hours = date.getHours();
-            let AmOrPm = hours >= 12 ? "pm" : "am";
-            hours = hours % 12 || 12;
-            if (hours < 10) {
-                hours = `0${hours}`;
-            }
-            let minutes = date.getMinutes();
-            if (minutes < 10) {
-                minutes = `0${minutes}`;
-            }
-            let month = months[date.getMonth()];
-            let year = date.getFullYear();
-            let day = date.getDate();
-            return `${weekDay}, ${month} ${day}, ${year}  ${hours}:${minutes} ${AmOrPm}`;
-        })
+export default function DateTime() {
+    let [date, setDate] = useState(newDate());
+    useEffect(() => {
+        let timer = setInterval(()=>setDate(new Date()), 1000 )
+        return function cleanup() {
+            clearInterval(timer)
+        }
+    
+    });
 
-function formatDay(timestamp) {
-  let date = new Date(timestamp * 1000);
-  let day = date.getDay();
-  let days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
-
-  return days[day];
+    return(
+        <div>
+        <p> Date : {date.toLocaleDateString()}</p>    
+        <p> Time : {date.toLocaleTimeString()}</p>
+        </div>
+    )
 }
-}
+
+  
