c1b2ea32a363860283d4d5e564890ac7be21c746
Ndrea3 <101579085+Ndrea3@users.noreply.github.com>
2022-06-26 16:55:31 -0400

    Updated the Search function to use the Weather API

--- src/Search.js
+++ src/Search.js
@@ -1,26 +1,43 @@
+import React, { useState } from "react";
+import axios from "axios";
+
 export default function Search() {
-  return (
-    <div className="weather-app">
-      <form id="search-form">
-        <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Enter a City..."
-              className="form-control"
-              id="city-input"
-              autoComplete="off"
-            />
-          </div>
-          <div className="col-3">
-            <input
-              type="submit"
-              value="Search"
-              className="btn btn-info w-100"
-            />
-          </div>
-        </div>
+  let [city, setCity] = useState("");
+  let [display, setDisplay] = useState(false);
+
+function displayWeather(response) {
+    console.log(response);
+    setDisplay(true);}
+
+  function showSubmit(event) {
+    event.preventDefault();
+    let apiKey = "f98ba7e599adf93cd93e20273e395b25";
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(url).then(displayWeather);
+  }
+    function updateCity(event) {
+      setCity(event.target.value);
+    }
+    let form = (
+      <form onSubmit={showSubmit}>
+        <input
+          type="search"
+          placeholder="Enter a city..."
+          onChange={updateCity}
+        />
+        <input type="submit" value="Search" onClick={showSubmit} />
       </form>
+    );
+  
+if (display) {
+  return (
+    <div>
+      {form}
+      <h2>{city}</h2>
     </div>
   );
+} else {
+  return form;
 }
+  }
+
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import App from "./App";
+import Search from "./Search";
+
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
@@ -9,5 +11,6 @@ const root = createRoot(rootElement);
 root.render(
   <StrictMode>
     <App />
+    <Search />
   </StrictMode>
 );
