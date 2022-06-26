1b46d9ff9801ef4d264298511470460104d756ad
Ndrea3 <101579085+Ndrea3@users.noreply.github.com>
2022-06-26 18:50:54 -0400

    Updated the way the date and time will display

--- src/Date.js
+++ src/Date.js
@@ -1,31 +1,20 @@
 import { useEffect, useState } from "react";
 
-
 export default function DateTime() {
-    let [date, setDate] = useState(new Date());
-    useEffect(() => {
-        let timer = setInterval(()=>setDate(new Date()), 1000 )
-        return function cleanup() {
-            clearInterval(timer)
-        }
-    
-    });
+  let [date, setDate] = useState(new Date());
+  useEffect(() => {
+    let timer = setInterval(() => setDate(new Date()), 1000);
+    return function cleanup() {
+      clearInterval(timer);
+    };
+  });
 
-    return(
-        <div>
-        <p> Date : {date.toLocaleDateString()}</p>    
-        <p> Time : {date.toLocaleTimeString()}</p>
-        </div>
-    )
+  return (
+    <div>
+      <p>
+        {" "}
+        Date/Time: {date.toLocaleDateString()}, {date.toLocaleTimeString()}
+      </p>
+    </div>
+  );
 }
-
-  export function getCurrentDate(separator = "") {
-    let newDate = new Date();
-    let date = newDate.getDate();
-    let month = newDate.getMonth() + 1;
-    let year = newDate.getFullYear();
-
-    return `${year}${separator}${
-      month < 10 ? `0${month}` : `${month}`
-    }${separator}${date}`;
-  }
