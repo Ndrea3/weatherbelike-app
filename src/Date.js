717c4f24eeab31aaa1f107dffab0ee26a384c55e
Ndrea3 <101579085+Ndrea3@users.noreply.github.com>
2022-06-26 18:45:15 -0400

    Testing another way to display the date

--- src/Date.js
+++ src/Date.js
@@ -19,4 +19,13 @@ export default function DateTime() {
     )
 }
 
-  
+  export function getCurrentDate(separator = "") {
+    let newDate = new Date();
+    let date = newDate.getDate();
+    let month = newDate.getMonth() + 1;
+    let year = newDate.getFullYear();
+
+    return `${year}${separator}${
+      month < 10 ? `0${month}` : `${month}`
+    }${separator}${date}`;
+  }
